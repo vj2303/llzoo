c30463db13183dca7b952b34c24ceaa794b79b81
Vishnu Jangid <103036831+vj2303@users.noreply.github.com>
2024-10-05 00:36:44 +0530

    Update Services.jsx

--- src/app/partner-details/[id]/Services.jsx
+++ src/app/partner-details/[id]/Services.jsx
@@ -1,6 +1,7 @@
 // components/ServicesSection.js
 import Image from 'next/image';
 import React from 'react';
+import Link from 'next/link';
 
 const ServicesSection = ({ services }) => {
     // const services = [
@@ -18,10 +19,12 @@ const ServicesSection = ({ services }) => {
       <h2 className="text-2xl sm:text-[32px] font-semibold mb-4">Services</h2>
       <div className="grid grid-cols-3 sm:grid-cols-5 gap-2">
         {services?.map((service) => (
-          <div key={service.id} className="text-center">
-            <Image src={service.image_url} alt={service.name} width={100} height={100} className="h-20 sm:h-40 sm:w-40 w-20 rounded-lg mx-auto" />
-            <p className="text-[14px] sm:text-[18px] mt-2">{service.name}</p>
-          </div>
+        <Link href={'/service-details'}>
+               <div key={service.id} className="text-center">
+                  <Image src={service.image_url} alt={service.name} width={100} height={100} className="h-20 sm:h-40 sm:w-40 w-20 rounded-lg mx-auto" />
+                  <p className="text-[14px] sm:text-[18px] mt-2">{service.name}</p>
+                </div>
+         </Link>
         ))}
       </div>
     </div>
